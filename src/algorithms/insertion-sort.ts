8fa1ddc9a6c9fb4628fd36db0d56fd3ea3b78c1c
Alvin Li <hins.xd@hotmail.com>
2022-03-29 05:18:11 +0800

    chore: insertion sort color

--- src/algorithms/insertion-sort.ts
+++ src/algorithms/insertion-sort.ts
@@ -4,9 +4,12 @@ import { getCompareColor } from "@/helpers/getCompareColors";
 export function* insertionSort(arr: number[]): Generator<StepState, StepState> {
   for (let i = 1; i < arr.length; i++) {
     let j = i;
-    console.log(getCompareColor(arr, j - 1, j));
+
     while (j >= 1) {
-      yield { result: arr, colors: getCompareColor(arr, j - 1, j) };
+      yield {
+        result: arr,
+        colors: { [i]: "blue", ...getCompareColor(arr, j - 1, j) },
+      };
       if (arr[j] > arr[j - 1]) {
         break;
       }
